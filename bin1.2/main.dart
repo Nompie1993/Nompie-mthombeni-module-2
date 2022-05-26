ec291fe246d99e00738f2fc6b10275d54813329e
NompieMthombeni <64683159+NompieMthombeni@users.noreply.github.com>
2022-05-26 16:57:48 +0200

    QUESTION 2

---
+++ bin1.2/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<String> AppName = [
+    'takealot, vula, dstv, fnb, pricecheck, RV, Pregnancy Journey, shyft, besmarta,'
+  ];
+
+  var WinningApp2017 = 'Shyt';
+
+  var WinningApp2018 = 'Besmarta';
+
+  int TotalNumber = 10;
+
+  print(TotalNumber);
+
+  print(WinningApp2017);
+  print(WinningApp2018);
+
+  print(AppName);
+}
